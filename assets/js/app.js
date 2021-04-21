731c9d56faed8f775dfe91aa405880e0c96148c3
Santiago Botta <sgobotta@gmail.com>
2021-04-21 00:24:08 -0300

    :computer: Implements the YT client api
    + Integrates a video player as a client phoenix hook

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+// Used to support async/wait functions
+import "regenerator-runtime/runtime"
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
@@ -17,20 +19,43 @@ import {Socket} from "phoenix"
 import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+import LoadYTIframeAPI from './deps/yt-iframe-api'
+import PlayerSyncing from "./hooks/player-syncing"
+import createPlayer from './lib/player'
+
+function onIframeReady() {
+  initLiveview()
+}
+
+function initPlayer(onStateChange, onVolumeChange) {
+  const playerContainer = document.getElementById("video-player")
+  return createPlayer(playerContainer, {onStateChange, onVolumeChange})
+}
+
+function initLiveview() {
+  let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+
+  const hooks = {
+    PlayerSyncing: PlayerSyncing(initPlayer),
+  }
+  
+  let liveSocket = new LiveSocket("/live", Socket, {
+    hooks,
+    params: {_csrf_token: csrfToken}
+  })
+  // connect if there are any LiveViews on the page
+  liveSocket.connect()
+  // expose liveSocket on window for web console debug logs and latency simulation:
+  // >> liveSocket.enableDebug()
+  // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+  // >> liveSocket.disableLatencySim()
+  window.liveSocket = liveSocket
+}
+
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
-// connect if there are any LiveViews on the page
-liveSocket.connect()
-
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+LoadYTIframeAPI(onIframeReady)
---
+++ assets/js/lib/player.js
@@ -0,0 +1,27 @@
+export default (container, {onStateChange}) => {
+  return new Promise((resolve) => {
+    new YT.Player(container, {
+      height: "100%",
+      width: "100%",
+      videoId: 'YPkp-oESVMM',
+      playerVars: {
+        controls: 0,
+        disablekb: 1,
+        enablejsapi: 1,
+        iv_load_policy: 3,
+        rel: 0,
+        showinfo: 0,
+      },
+      events: {
+        onReady: event => {
+          const player = event.target
+          resolve(player)
+        },
+        onStateChange,
+        onError: (error) => {
+          console.error('[YT] ', error)
+        }
+      },
+    })
+  })
+}
