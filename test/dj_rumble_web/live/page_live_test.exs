ea08fde905da44912db6303166b8a3c9c3f7a805
Santiago <santiago@camba.coop>
2021-04-24 02:54:29 -0300

    :test_tube: Fixes new live view tests

--- test/dj_rumble_web/live/page_live_test.exs
+++ test/dj_rumble_web/live/page_live_test.exs
@@ -5,7 +5,7 @@ defmodule DjRumbleWeb.PageLiveTest do
 
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Listing Rooms"
-    assert render(page_live) =~ "Listing Rooms"
+    assert disconnected_html =~ "Dj Rooms"
+    assert render(page_live) =~ "Dj Rooms"
   end
 end
