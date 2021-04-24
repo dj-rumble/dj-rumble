fa66e865bc206d1759f8c284d80ada66d1ff0c4d
h3adHunter <juanmanuelbaez1994@gmail.com>
2021-04-24 02:48:58 -0300

    Add ConnectedUsers's Component

---
+++ lib/dj_rumble_web/live/components/connected_users/connected_users.ex
@@ -0,0 +1,14 @@
+defmodule DjRumbleWeb.Live.Components.ConnectedUsers do
+  @moduledoc """
+  Responsible for showing the active users in the room
+  """
+
+  use DjRumbleWeb, :live_component
+
+  def update(assigns, socket) do
+    {:ok,
+      socket
+      |> assign(assigns)
+    }
+  end
+end
