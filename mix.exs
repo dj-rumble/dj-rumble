4211babab42e7e0159e62ddd25953ba87f6d4247
h3adHunter <juanmanuelbaez1994@gmail.com>
2021-04-24 03:34:26 -0300

    Adds Faker for random user names

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule DjRumble.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:faker, "~> 0.16"}
     ]
   end
 
