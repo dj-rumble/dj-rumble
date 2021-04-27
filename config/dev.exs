11095bd8caafee32acd522495d77c3e014cd6d2b
Santiago <santiago@camba.coop>
2021-04-27 20:07:12 -0300

    :wrench: Updates environments configuration to use env vars

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :dj_rumble, DjRumble.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "dj_rumble_dev",
-  hostname: "localhost",
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
+  database: System.get_env("DB_DATABASE"),
+  hostname: System.get_env("DB_HOSTNAME"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
