7155ac7e1000f44e83f43f9d08ee8565e3095a57
h3adHunter <juanmanuelbaez1994@gmail.com>
2021-04-28 18:00:24 -0300

    Agrega nuevos rooms y videos precargados y los relaciona en rooms_videos

--- priv/repo/seeds/rooms_videos.exs
+++ priv/repo/seeds/rooms_videos.exs
@@ -8,9 +8,16 @@ schema_plural = "rooms videos"
 try do
   rooms = Rooms.list_rooms()
   videos = Rooms.list_videos()
+  single_video_rooms = Enum.with_index(Enum.take(rooms, 3))
+  multi_video_room = Enum.at(rooms, -1)
+  vulf_videos = Enum.with_index(Enum.take(videos, -6))
 
-  for {room, index} <- Enum.with_index(rooms) do
-    video = Enum.at(videos, index)
+  for {video, video_index} <- vulf_videos do
+    Rooms.create_room_video(%{room_id: multi_video_room.id, video_id: video.id})
+  end
+
+  for {room, room_index} <- single_video_rooms do
+    video = Enum.at(videos, room_index)
     Rooms.create_room_video(%{room_id: room.id, video_id: video.id})
   end
   |> length()
