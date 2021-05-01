24deb27087322931656957659888c85e16a1117a
Santiago <santiago@camba.coop>
2021-05-01 17:22:34 -0300

    :wrench: Adds the phoenix_inline_svg dependency

--- lib/dj_rumble_web.ex
+++ lib/dj_rumble_web.ex
@@ -80,6 +80,7 @@ defmodule DjRumbleWeb do
     quote do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
+      use PhoenixInlineSvg.Helpers
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
