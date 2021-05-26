592cd87dd0da532665347a42b9a16c5b596ba9ad
Santiago Botta <sgobotta@gmail.com>
2021-05-25 23:40:05 -0300

    :wrench: Ignores supervisor callback mismatches

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,6 @@
 [
   {":0:unknown_function Function ExUnit.Callbacks.__merge__/3 does not exist."},
-  {":0:unknown_function Function ExUnit.CaseTemplate.__proxy__/2 does not exist."}
+  {":0:unknown_function Function ExUnit.CaseTemplate.__proxy__/2 does not exist."},
+  {"lib/dj_rumble/rooms/supervisor.ex", :callback_type_mismatch, 14},
+  {"lib/dj_rumble/rounds/supervisor.ex", :callback_type_mismatch, 14}
 ]
