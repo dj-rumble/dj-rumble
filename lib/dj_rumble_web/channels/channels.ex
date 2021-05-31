5814a818ee682b8977ccf60ee6aedb13b846247e
Santiago <santiago@camba.coop>
2021-05-31 01:08:22 -0300

    :computer: Implements a Channels module

---
+++ lib/dj_rumble_web/channels/channels.ex
@@ -0,0 +1,24 @@
+defmodule DjRumbleWeb.Channels do
+  @moduledoc """
+  Responsible for declaring channel topics and subscriptions management.
+  """
+  @room_topic "room:<slug>"
+  @player_is_ready_topic "room:<slug>:ready"
+  @matchmaking_details_request_topic "matchmaking:<slug>:waiting_for_details"
+
+  def get_topic(:room), do: @room_topic
+
+  def get_topic(:player_is_ready), do: @player_is_ready_topic
+
+  def get_topic(:matchmaking_details_request), do: @matchmaking_details_request_topic
+
+  def get_topic(type, slug), do: String.replace(get_topic(type), "<slug>", slug)
+
+  def subscribe(type, slug) do
+    Phoenix.PubSub.subscribe(DjRumble.PubSub, get_topic(type, slug))
+  end
+
+  def unsubscribe(type, slug) do
+    Phoenix.PubSub.unsubscribe(DjRumble.PubSub, get_topic(type, slug))
+  end
+end
