7f807d19d114e1b1b0cf1de573dc1ddfe3182916
Santiago Botta <sgobotta@gmail.com>
2021-06-13 23:14:08 -0300

    :wrench: Updates tailwind config and an eslintrc rule

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -11,9 +11,12 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        'inner-button': 'inset 0px 0px 20px #323637, inset -2px 0px 2px #323637 !important',
-        'inner-input': 'inset -4px -4px 10px #323637, inset 4px 4px 10px #080a0a',
-        'card': '0px 0px 0px #000000, 4px 4px 4px #000000',
+        'card':
+          '0px 0px 0px #000000, 4px 4px 4px #000000',
+        'inner-button':
+          'inset 0px 0px 20px #323637, inset -2px 0px 2px #323637 !important',
+        'inner-input':
+          'inset -4px -4px 10px #323637, inset 4px 4px 10px #080a0a'
       }
     },
     fontFamily: {
@@ -22,6 +25,8 @@ module.exports = {
     }
   },
   variants: {
-    extend: {},
-  },
+    extend: {
+      scale: ['focus-within']
+    }
+  }
 }
