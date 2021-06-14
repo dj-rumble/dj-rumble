dcc00a6d54970a080cca86a7f247a48ec417979e
Santiago Botta <sgobotta@gmail.com>
2021-06-14 00:42:49 -0300

    :computer: Triggers the registered modal when spectators try to search

--- lib/dj_rumble_web/live/components/search_fab/search_fab.ex
+++ lib/dj_rumble_web/live/components/search_fab/search_fab.ex
@@ -7,8 +7,18 @@ defmodule DjRumbleWeb.Live.Components.SearchFab do
 
   @impl true
   def update(assigns, socket) do
+
+    {click_event, event_target} = case assigns.is_search_enabled do
+      true ->
+        {"open", "djrumble-searchbox-modal-menu"}
+      false ->
+        {"open", "djrumble-register-modal-menu"}
+    end
+
     {:ok,
      socket
-     |> assign(assigns)}
+     |> assign(:id, assigns.id)
+     |> assign(:click_event, click_event)
+     |> assign(:event_target, event_target)}
   end
 end
