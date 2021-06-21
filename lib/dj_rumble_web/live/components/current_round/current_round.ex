d2bc8a847fc90e607fa6ef8da7ea1b04fd3aca41
Santiago <santiago@camba.coop>
2021-06-21 02:47:29 -0300

    :computer: Adds an equalizer animation current round details

--- lib/dj_rumble_web/live/components/current_round/current_round.ex
+++ lib/dj_rumble_web/live/components/current_round/current_round.ex
@@ -5,12 +5,31 @@ defmodule DjRumbleWeb.Live.Components.CurrentRound do
 
   use DjRumbleWeb, :live_component
 
+  def mount(socket) do
+    {:ok,
+     socket
+     |> assign(:is_playing, false)}
+  end
+
   def update(assigns, socket) do
     %{current_round: current_round} = assigns
 
+    %{video: video} = current_round
+
+    is_playing_video = video.video_id != "placeholder"
+
     {:ok,
      socket
+     |> assign(:is_playing, is_playing_video)
      |> assign(:current_round, current_round)}
+  end
+
+  defp render_title(title, true, _assigns), do: title
+
+  defp render_title(title, false, assigns) do
+    ~L"""
+    <span class="animate-pulse text-yellow-800"><%= title %></span>
+    """
   end
 
   defp render_dj(current_round, assigns) do
@@ -26,4 +45,10 @@ defmodule DjRumbleWeb.Live.Components.CurrentRound do
         """
     end
   end
+
+  defp render_equalizer(true) do
+    Phoenix.View.render(DjRumbleWeb.EqualizerView, "equalizer.html", extra_classes: "left-4")
+  end
+
+  defp render_equalizer(false), do: ""
 end
