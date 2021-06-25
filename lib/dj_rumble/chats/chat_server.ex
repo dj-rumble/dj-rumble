f158f45d32c73adbe5076c789697e69a7e83fccd
Santiago <santiago@camba.coop>
2021-06-25 00:24:56 -0300

    :test_tube: Tests a ChatServer gets a list messages

--- lib/dj_rumble/chats/chat_server.ex
+++ lib/dj_rumble/chats/chat_server.ex
@@ -16,6 +16,10 @@ defmodule DjRumble.Chats.ChatServer do
     GenServer.call(pid, :get_state)
   end
 
+  def get_messages(pid, from) do
+    GenServer.cast(pid, {:get_messages, from})
+  end
+
   def new_message(pid, user, message) do
     GenServer.cast(pid, {:new_message, user, message})
   end
@@ -38,11 +42,17 @@ defmodule DjRumble.Chats.ChatServer do
   end
 
   @impl GenServer
+  def handle_cast({:get_messages, from}, %{messages: messages} = state) do
+    :ok = Process.send(from, {:receive_messages, messages}, [])
+    {:noreply, state}
+  end
+
+  @impl GenServer
   def handle_cast({:new_message, user, message}, state) do
     user_message = %{user: user, message: message}
     state = %{state | messages: state.messages ++ [user_message]}
 
-    :ok = Channels.broadcast(state.chat_topic, {:receive_chat_message, user_message})
+    :ok = Channels.broadcast(state.chat_topic, {:receive_new_message, user_message})
 
     {:noreply, state}
   end
